fc1a444b44fb003c76e02a706b1bc5e6e5642f7b
Anna <anya.lechenko@mail.ru>
2025-03-10 19:29:24 +0300

    update tost

--- app/src/main/java/com/annalech/weather/presentation/firstPage/FirstPageActivity.kt
+++ app/src/main/java/com/annalech/weather/presentation/firstPage/FirstPageActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.widget.doOnTextChanged
 import com.annalech.weather.R
 import com.annalech.weather.databinding.ActivityFirstPageBinding
 import com.annalech.weather.presentation.secondPage.SecondPageActivity
+import kotlinx.coroutines.delay
 
 class FirstPageActivity : AppCompatActivity() {
     private lateinit var   binding:ActivityFirstPageBinding
@@ -18,25 +19,16 @@ class FirstPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFirstPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.button.isEnabled = false
 
         binding.etEditCity.doOnTextChanged { text, _, _, _ ->
-            val city = text.toString()
-            // Проверяем, что город состоит только из латинских букв
-            if (!city.matches("^[a-zA-Z]+$".toRegex())) {
-                // Если не соответствует условию, показываем тост
-                binding.button.isEnabled = false
-                Toast.makeText(this,
-                    "Город должен быть написан латиницей и не содержать чисел.",
-                    Toast.LENGTH_SHORT)
-                    .show()
 
-            }
-            binding.button.isEnabled = true
+            val info = text.toString()
+            val city = checkInfo(info)
         }
 
         binding.button.setOnClickListener {
-            val info = binding.etEditCity.text.toString()
+           val info = binding.etEditCity.text.toString()
            val city =  checkInfo(info)
            startActivity( SecondPageActivity.getIntent(this, city)) //запуск второго окна
         }
@@ -48,17 +40,29 @@ class FirstPageActivity : AppCompatActivity() {
     private fun checkInfo( info:String):String{
        val city = info.trim()
             // Проверяем, что город состоит только из латинских букв
-            if (!city.matches("^[a-zA-Z]+$".toRegex())) {
+            if(!city.matches("^[a-zA-Z]+$".toRegex())) {
                 // Если не соответствует условию, показываем тост
                 Toast.makeText(this,
                     "Город должен быть написан латиницей и не содержать чисел.",
                     Toast.LENGTH_SHORT).show()
                       binding.button.isEnabled = false
-            }
+            }else if (city.length < MIN_LENGHT_CITY) {
+                // Если не соответствует условию, показываем тост
+                binding.button.isEnabled = false
+                Toast.makeText(this,
+                    "Город должен быть из 2 и более символов.",
+                    Toast.LENGTH_SHORT).show()
 
-        binding.button.isEnabled = true
+            } else{
+                binding.button.isEnabled = true
+            }
        return city
     }
 
+
+
+    companion object{
+        private const val MIN_LENGHT_CITY = 3
+    }
 }
 
