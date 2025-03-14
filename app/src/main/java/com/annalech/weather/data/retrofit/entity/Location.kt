5884148a62eb1e59b4f768472bc436fa5a5ff825
Anna <anya.lechenko@mail.ru>
2025-03-14 22:01:48 +0300

    add parcelize . add fragment argument

--- app/src/main/java/com/annalech/weather/data/retrofit/entity/Location.kt
+++ app/src/main/java/com/annalech/weather/data/retrofit/entity/Location.kt
@@ -1,13 +1,15 @@
 package com.annalech.weather.data.retrofit.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonIgnoreUnknownKeys
-
+@Parcelize
 @Serializable
 @JsonIgnoreUnknownKeys
 data class Location(
     @SerialName("name")val name: String,
     @SerialName("region")   val region: String,
     @SerialName("country")  val country: String
-)
+):Parcelable
--- app/src/main/java/com/annalech/weather/presentation/secondPage/SuccessLoadFragment.kt
+++ app/src/main/java/com/annalech/weather/presentation/secondPage/SuccessLoadFragment.kt
@@ -4,18 +4,37 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.annalech.weather.R
+import com.annalech.weather.data.retrofit.entity.ResponseWeather
 import com.annalech.weather.databinding.SuccessLoadFragmentBinding
 
 class SuccessLoadFragment  : Fragment(R.layout.success_load_fragment){
     private var binding: SuccessLoadFragmentBinding? = null
+    private lateinit var argument: ResponseWeather
+
+///get city
+    override fun onCreate(savedInstanceState: Bundle?) {
+        requireArguments().getParcelable<ResponseWeather>(WEATHER_RESPONSE)?.let {
+            it->
+            argument = it
+        }
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = SuccessLoadFragmentBinding.bind(view)
-
+        binding?.let {
+           it.tvCity.text = argument.location.name
+        }
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    companion object{
+        const val WEATHER_RESPONSE = "weather"
     }
 }
